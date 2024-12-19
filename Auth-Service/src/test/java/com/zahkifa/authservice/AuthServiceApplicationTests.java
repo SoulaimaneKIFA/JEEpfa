4af861a1c86a534a148d4c32c281cd79278233e5
SoulaimaneKIFA <soulaimanes75@gmail.com>
2024-12-19 21:04:12 +0100

    Today's progress

--- Auth-Service/src/test/java/com/zahkifa/authservice/AuthServiceApplicationTests.java
+++ Auth-Service/src/test/java/com/zahkifa/authservice/AuthServiceApplicationTests.java
@@ -1,6 +1,6 @@
 package com.zahkifa.authservice;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
